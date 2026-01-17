d145caf721010effb138e17e5a07052770af1234
ilmauliaa_ <ilmaaulia1408@gmail.com>
2026-01-17 08:02:11 +0700

    Add geometric shape area and perimeter calculator
    Implement a program to calculate the perimeter and area of various geometric shapes including square, rectangle, triangle, and circle.

---
+++ Terminal BangunDatar.py
@@ -0,0 +1,60 @@
+PS C:\Users\Dell\OneDrive\Documents\belajar pemrograman> & C:/Users/Dell/AppData/Local/Programs/Python/Python314/python.exe "c:/Users/Dell/OneDrive/Documents/belajar pemrograman/BangunDatar.py"
+Program Menghitung Keliling  & Luas Bngun Datar
+-----------------------------------------------
+[1] Keliling dan Luas Persegi
+[2] Keliling dan Luas Persegi Panjang
+[3] Keliling dan Luas Segitiga
+[4] Keliling dan Luas Lingkaran
+-----------------------------------------------
+Pilih Salah Satu Program (1-4):1
+Program Menghitung Keliling & Luas Persegi 
+Masukkan nilai Sisi :8
+
+Luasnya =  64
+keliling = 32
+PS C:\Users\Dell\OneDrive\Documents\belajar pemrograman> & C:/Users/Dell/AppData/Local/Programs/Python/Python314/python.exe "c:/Users/Dell/OneDrive/Documents/belajar pemrograman/BangunDatar.py"
+Program Menghitung Keliling  & Luas Bngun Datar
+-----------------------------------------------
+[1] Keliling dan Luas Persegi
+[2] Keliling dan Luas Persegi Panjang
+[3] Keliling dan Luas Segitiga
+[4] Keliling dan Luas Lingkaran
+-----------------------------------------------
+Pilih Salah Satu Program (1-4):2
+Program Menghitung Keliling & Luas Persegi Panjang
+--------------------------------------------------
+Masukkan nilai lebar :15
+
+Luasnya = 150
+Keliling = 50
+PS C:\Users\Dell\OneDrive\Documents\belajar pemrograman> & C:/Users/Dell/AppData/Local/Programs/Python/Python314/python.exe "c:/Users/Dell/OneDrive/Documents/belajar pemrograman/BangunDatar.py"
+Program Menghitung Keliling  & Luas Bngun Datar
+-----------------------------------------------
+[1] Keliling dan Luas Persegi
+[2] Keliling dan Luas Persegi Panjang
+[3] Keliling dan Luas Segitiga
+[4] Keliling dan Luas Lingkaran
+-----------------------------------------------
+Pilih Salah Satu Program (1-4):3
+rogram Menghitung Keliling & Luas Segitiga
+------------------------------------------
+Masukkan nilai Alas :10
+Masukkan nilai Tinggi :18
+
+Luasnya = (0, 900.0)
+Keliling = 30.0
+PS C:\Users\Dell\OneDrive\Documents\belajar pemrograman> & C:/Users/Dell/AppData/Local/Programs/Python/Python314/python.exe "c:/Users/Dell/OneDrive/Documents/belajar pemrograman/BangunDatar.py"
+Program Menghitung Keliling  & Luas Bngun Datar
+-----------------------------------------------
+[1] Keliling dan Luas Persegi
+[2] Keliling dan Luas Persegi Panjang
+[3] Keliling dan Luas Segitiga
+[4] Keliling dan Luas Lingkaran
+-----------------------------------------------
+Pilih Salah Satu Program (1-4):4
+rogram Menghitung Keliling & Luas Lingkaran
+-------------------------------------------
+Masukkan nilai Jri-jari :8
+
+Luasnya = 200.96
+Keliling = 50.24
